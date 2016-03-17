3bb9f9a4100f5d8078743a334f04f7aad743786b
koziscool <kozjob@yahoo.com>
2016-03-17 17:04:59 -0500

    first cut pupply list

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ var breedlist = (function($){
 
 })($);
 
+
+var handlePuppy = function( puppyObj, $puppyList ) {
+  var name = puppyObj.name;
+  var breed = puppyObj.breed.name;
+  var createdAt = puppyObj.created_at;
+  var $listElt = $('<li>' + name + ' (' + breed + ') ' + createdAt + '</li>');
+  $puppyList.append( $listElt);
+};
+
+var updatePuppyResponse = function( data ) {
+  // console.dir(data);
+  var $puppyList = $('.puppy-list')
+  data.forEach( function( puppyObj ) {
+    handlePuppy(puppyObj, $puppyList );
+  } )
+};
+
+var updatePuppyRequest = function(e) {
+  e.preventDefault();
+  console.log('im here');
+  var url = "https://ajax-puppies.herokuapp.com/puppies.json";
+  var method = "GET";
+
+  $.get( url, {}, updatePuppyResponse);
+};
+
+$(document).ready( function(){
+  $("#refresh-puppies").on('click', updatePuppyRequest);
+});
